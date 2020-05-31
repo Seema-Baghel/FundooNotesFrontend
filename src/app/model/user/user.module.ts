f234be42143b3ce58426fedf6fd3ba46b0245575
Seema-Baghel <seemabaghel1997@gmail.com>
2020-05-31 21:47:38 +0530

    [Seema Baghel]Add. Added user model for fundoo notes

---
+++ src/app/model/user/user.module.ts
@@ -0,0 +1,16 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+export class UserModule {
+  username: string;
+  password: string;
+   //Users: [];
+ }
