1b40bc7ee1b06ce033da72bef81cda685abb6ccf
Seema-Baghel <seemabaghel1997@gmail.com>
2020-05-31 21:41:10 +0530

    [Seema Baghel]Add. Added login UI

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserserviceService } from 'src/app/services/userservice.service';
+import { HttpserviceService } from 'src/app/services/httpservice.service';
+import { environment } from 'src/environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserModule } from 'src/app/model/user/user.module';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit{
+  user: UserModule  = new UserModule();
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  hide = true;
+
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
+    private router: Router, private userService: UserserviceService,
+    private httpUtil: HttpserviceService, private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit(user) {
+
+    if (this.loginForm.invalid) {
+      console.log("form validation failed");
+      return;
+    }
+    this.userService.login(this.loginForm.value).subscribe(response => {
+      console.log("login true");
+      localStorage.setItem('token', response.token);
+      /**this.router.navigate(['/dashboard']);*/
+      this.snackBar.open("Successfully logged In", "Ok", { duration: 2000 })
+    }, error => {
+      this.snackBar.open("error in login", "please enter valid data", { duration: 2000 })
+    });
+  }
+}
